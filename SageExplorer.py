3692731f83d4b7b7d32bc1b88f375745a8340849
Zerline <odile.benassy@u-psud.fr>
2018-06-24 11:17:17 +0200

    Debug du précédent.

--- SageExplorer.py
+++ SageExplorer.py
@@ -16,6 +16,7 @@ AUTHORS:
 from ipywidgets import Layout, VBox, HBox, Text, Label, HTML, Select, Textarea, Accordion, Tab
 import traitlets
 from inspect import getdoc, getmembers, ismethod, isbuiltin
+from sage.combinat.rooted_tree import LabelledRootedTree
 
 cell_layout = Layout(width='3em',height='2em', margin='0',padding='0')
 box_layout = Layout()
@@ -39,7 +40,7 @@ def class_hierarchy(c):
     sage: hierarchy(P.__class__)
     <class 'sage.combinat.partition.Partitions_n_with_category.element_class'>[<class 'sage.combinat.partition.Partition'>[<class 'sage.combinat.combinat.CombinatorialElement'>[<class 'sage.combinat.combinat.CombinatorialObject'>[<type 'sage.structure.sage_object.SageObject'>[<type 'object'>[]]], <type 'sage.structure.element.Element'>[<type 'sage.structure.sage_object.SageObject'>[<type 'object'>[]]]]], <class 'sage.categories.finite_enumerated_sets.FiniteEnumeratedSets.element_class'>[<class 'sage.categories.enumerated_sets.EnumeratedSets.element_class'>[<class 'sage.categories.sets_cat.Sets.element_class'>[<class 'sage.categories.sets_with_partial_maps.SetsWithPartialMaps.element_class'>[<class 'sage.categories.objects.Objects.element_class'>[<type 'object'>[]]]]], <class 'sage.categories.finite_sets.FiniteSets.element_class'>[<class 'sage.categories.sets_cat.Sets.element_class'>[<class 'sage.categories.sets_with_partial_maps.SetsWithPartialMaps.element_class'>[<class 'sage.categories.objects.Objects.element_class'>[<type 'object'>[]]]]]]]
     """
-    return LabelledRootedTree([ hierarchy(b) for b in c.__bases__], label=c)
+    return LabelledRootedTree([ class_hierarchy(b) for b in c.__bases__], label=c)
 
 
 class SageExplorer(VBox):
@@ -57,13 +58,19 @@ class SageExplorer(VBox):
         super(SageExplorer, self).__init__()
         self.obj = obj
         self.members = [x for x in getmembers(obj) if not x[0].startswith('_') and not 'deprecated' in str(type(x[1])).lower()]
-        ct = hierarchy(obj.__class__)
+        ct = class_hierarchy(obj.__class__)
+        #print ct
         basemembers = {}
         globbasemembers = []
-        for c in ct.pre_order_traversal():
+        traversal = ct.pre_order_traversal_iter()
+        while 1:
+            next = traversal.next()
+            if not next:
+                break
+            c = next.label()
             basemembers[c] = []
             for m in self.members:
-                if m[0] in [x[0] for x in c.getmembers()]:
+                if m[0] in [x[0] for x in getmembers(c)]:
                     basemembers[c].append(m)
                     if c != obj.__class__ and not m in globbasemembers:
                         globbasemembers.append(m)
@@ -72,7 +79,7 @@ class SageExplorer(VBox):
         self.builtins = [x for x in self.members if isbuiltin(x[1])]
         menus = []
         menus.append(Select(options = [('Object methods:', None)] + [x for x in self.methods if not x in globbasemembers]))
-        menus.append(Select(options = [('Parent methods:', None)] + [x for x in self.merhods if x in globbasemembers]))
+        menus.append(Select(options = [('Parent methods:', None)] + [x for x in self.methods if x in globbasemembers]))
         menus.append(Select(options = [('Builtins:', None)] + self.builtins))
         self.title = Label(str(type(obj)))
         self.visual = Textarea(obj._repr_diagram())
