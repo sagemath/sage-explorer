3055b996195895b5d858b9ba1c8b1c4bd4276f86
Zerline <odile.benassy@u-psud.fr>
2018-07-21 19:53:39 +0200

    Importing sage_combinat_widgets (fix).

--- sage_explorer/_widgets.py
+++ sage_explorer/_widgets.py
@@ -2,14 +2,14 @@
 
 from sage.all import *
 try:
-    from sage_combinat_widgets import *
+    import sage_combinat_widgets
 except:
     pass
 
 sage.schemes.curves.curve.Curve_generic._widget_ = 'PlotWidget'
 Tableau._widget_ = 'sage_combinat_widgets.TableauWidget'
-SemistandardTableau._widget_ = 'sage-combinat-widgets.SemistandardTableauWidget'
-StandardTableau._widget_ = 'sage-combinat-widgets.StandardTableauWidget'
-Partition._widget_ = 'sage-combinat-widgets.PartitionWidget'
-graphs.GridGraph._widget_ = 'sage-combinat-widgets.DominosWidget'
-graphs.AztecDiamondGraph._widget_ = 'sage-combinat-widgets.DominosWidget'
+SemistandardTableau._widget_ = 'sage_combinat_widgets.SemistandardTableauWidget'
+StandardTableau._widget_ = 'sage_combinat_widgets.StandardTableauWidget'
+Partition._widget_ = 'sage_combinat_widgets.PartitionWidget'
+graphs.GridGraph._widget_ = 'sage_combinat_widgets.DominosWidget'
+graphs.AztecDiamondGraph._widget_ = 'sage_combinat_widgets.DominosWidget'
