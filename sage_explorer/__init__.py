6ed8e181e4b96de69af82c9e65096a6d37bb493a
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-07-22 22:20:39 +0200

    Preparatory work for using Recursive Monkey Patch; not used for now

--- sage_explorer/__init__.py
+++ sage_explorer/__init__.py
@@ -1,1 +1,8 @@
 from .sage_explorer import SageExplorer
+# # Patch the Sage library
+# import sys, logging
+# from recursive_monkey_patch import monkey_patch
+# sage_explorer = sys.modules[__name__]
+# import misc
+# import sage
+# monkey_patch(sage_explorer.misc, sage.misc, log_level=logging.INFO)
