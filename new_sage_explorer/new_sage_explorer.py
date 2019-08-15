e67d7a5d1f6855f9f248037cb402a3dd89b256f2
Zerline <odile.benassy@u-psud.fr>
2019-08-15 20:57:03 +0200

    Building up the new explorer.

--- new_sage_explorer/new_sage_explorer.py
+++ new_sage_explorer/new_sage_explorer.py
@@ -13,7 +13,7 @@ AUTHORS:
 - Odile Bénassy, Nicolas Thiéry, Nathan Carter
 
 """
-from ipywidgets import Box, HBox, VBox, GridBox, Label, Layout, Text, HTML, HTMLMath, Button, Combobox
+from ipywidgets import Box, HBox, VBox, GridBox, Label, Layout, Text, HTML, HTMLMath, Accordion, Button, Combobox
 from ipywidgets.widgets.widget_description import DescriptionStyle
 from traitlets import Any
 from ipywidgets.widgets.trait_types import InstanceDict, Color
@@ -25,8 +25,15 @@ title_layout = Layout(width='100%', padding='12px')
 css_lines = []
 css_lines.append(".title-level2 {font-size: 150%}")
 css_lines.append(".explorer-title {background-color: teal}")
-css_lines.append(".explorable-value {background-color: #999}")
+css_lines.append(".explorable-value {background-color: #ccc}")
 css = HTML("<style>%s</style>" % '\n'.join(css_lines))
+
+try:
+    ip = get_ipython()
+    ip.display_formatter.format(css)
+except:
+    pass # We are in the test environment
+
 
 def get_visual_widget(obj):
     r"""
@@ -70,6 +77,19 @@ class MathTitle(HTMLMath):
         self.add_class("title-level%d" % level)
 
 
+class Separator(Label):
+    r"""
+    A separator with a letter ot symbol in it.
+    """
+
+    def __init__(self, s):
+        super(Separator, self).__init__(
+            s,
+            layout=Layout(padding='0 4px')
+        )
+        self.add_class("separator")
+
+
 class ExplorerTitle(Box):
     r"""The sage explorer title bar
     """
@@ -78,12 +98,27 @@ class ExplorerTitle(Box):
     def __init__(self, obj):
         s = "Exploring: %s" % str(obj)
         #d = [l for l in obj.__doc__.split("\n") if l][0].strip()
-        super(ExplorerTitle, self).__init__((MathTitle(s, 2),))
+        super(ExplorerTitle, self).__init__(
+            (MathTitle(s, 2),),
+            layout=Layout(padding='5px 10px')
+        )
         self.add_class("explorer-title")
         self.value = obj
 
 
-class ExplorableValue(Box):
+class ExplorerDescription(Box):
+    r"""The sage explorer object description
+    """
+    value = Any()
+
+    def __init__(self, obj):
+        self.value = obj
+        s = [l for l in obj.__doc__.split("\n") if l][0].strip()
+        super(ExplorerDescription, self).__init__((HTMLMath(s),))
+        self.add_class("explorer-description")
+
+
+class ExplorableValue(HBox):
     r"""
     A repr string with a link for a Sage object.
     FIXME will be a DOMWidget or HTML, with a specific javascript View
@@ -99,31 +134,37 @@ class ExplorableValue(Box):
         else:
             s = obj.__str__()
         h = HTMLMath('$$%s$$' % s)
+        h.add_class('explorable-value')
         l0 = Label(" ")
-        l = Label("Output here")
+        l = Label("clc")
         def handle_clic(e):
             l.value = str(obj)
         clic = Event(source=h, watched_events=['click'])
         clic.on_dom_event(handle_clic)
-        super(ExplorableValue, self).__init__((h, l0, l))
-        self.add_class('explorable-value')
+        super(ExplorableValue, self).__init__(
+            (h, l0, l),
+            layout = Layout(border='1px solid green', padding='2px 50px 2px 2px')
+        )
 
 
-class ExplorerProperties(GridBox):
+class ExplorerProperties(Box):
     r"""
     Display object properties as a table.
     """
     value = Any()
 
     def __init__(self, obj):
+        self.value = obj
         children = []
         for p in get_properties(obj):
             val = getattr(obj, p.name).__call__()
             children.append(Label(p.prop_label))
             children.append(ExplorableValue(val))
-            children.append(Label("?"))
-        super(ExplorerProperties, self).__init__(children, layout=Layout(grid_template_columns='40% 40% 20%'))
-        self.value = obj
+            #children.append(Label("?"))
+        super(ExplorerProperties, self).__init__(
+            (GridBox(children, layout=Layout(grid_template_columns='25% 75%')),),
+            layout=Layout(border='1px solid red')
+            )
 
 
 class ExplorerVisual(Box):
@@ -133,8 +174,8 @@ class ExplorerVisual(Box):
     value = Any()
 
     def __init__(self, obj):
-        super(ExplorerVisual, self).__init__()
         self.value = obj
+        super(ExplorerVisual, self).__init__(layout = Layout(border='1px solid red'))
         w = get_visual_widget(obj)
         if w:
             self.children = [w]
@@ -165,15 +206,18 @@ class ExplorerMenus(Box):
         self.value = obj
 
 
-class ExplorerNaming(Text):
+class ExplorerNaming(Box):
     r"""
     A text input to give a name
     """
     value = Any()
 
     def __init__(self, obj, label="x"):
-        super(ExplorerNaming, self).__init__(label)
         self.value = obj
+        super(ExplorerNaming, self).__init__(
+            (Text(label),),
+            layout=Layout(width='25px')
+        )
 
     def update(self, label):
         r"""
@@ -213,7 +257,18 @@ class ExplorerInput(Text):
         self.value = obj
 
 
-class ExplorerHelp(Box):
+class ExplorerOutput(Box):
+    r"""
+    A text input to input method arguments
+    """
+    value = Any()
+
+    def __init__(self, obj=None):
+        self.value = obj
+        super(ExplorerOutput, self).__init__((HTMLMath("&nbsp;"),))
+
+
+class ExplorerHelp(Accordion):
     r"""
     Contains help, or output + help as expandable
     Contains MathJax
@@ -222,9 +277,13 @@ class ExplorerHelp(Box):
 
     def __init__(self, obj):
         self.value = obj
-        #t = HTMLMath("$$%s$$" % str(obj))
         t = HTMLMath("$$%s$$" % obj.__doc__)
-        super(ExplorerHelp, self).__init__((t,))
+        super(ExplorerHelp, self).__init__(
+            (t,),
+            selected_index=None,
+            layout=Layout(width='95%', border='1px solid yellow', padding='0')
+        )
+        self.set_title(0, "Help")
 
 
 class NewSageExplorer(VBox):
@@ -242,24 +301,43 @@ class NewSageExplorer(VBox):
         """
         super(NewSageExplorer, self).__init__()
         self.titlebox = ExplorerTitle(obj)
-        self.propsbox = ExplorerProperties(obj)
+        self.propsbox = VBox([ExplorerDescription(obj), ExplorerProperties(obj)])
         self.titlebox.add_class('titlebox')
         self.titlebox.add_class('lightborder')
         self.visualbox = ExplorerVisual(obj)
         self.visualbox.add_class('visualbox')
 
-        self.top = GridBox([self.titlebox, self.visualbox, self.propsbox], layout=Layout(grid_template_columns='60% 40%'))
+        self.top = VBox(
+            [self.titlebox,
+             HBox(
+                 [self.propsbox, Separator(' '), self.visualbox],
+                         layout=Layout(margin='10px 0')
+             )
+            ],
+            layout=Layout(border='1px solid yellow')
+        )
 
         self.menusbox = ExplorerMenus(obj)
         self.namingbox = ExplorerNaming(obj)
         self.searchbox = ExplorerMethodSearch(obj)
         self.inputbox = ExplorerInput()
-        self.inputbutton = Button(description='-')
+        self.inputbutton = Button(description='-', layout=Layout(width='30px'))
         self.gobutton = Button(description='Run!', tooltip='Run the function or method, with specified arguments')
-        self.actionbox = HBox([self.namingbox, Label('.'), self.searchbox, Label('('), self.inputbox, self.inputbutton, Label(')'), self.gobutton])
-        self.outputbox = ExplorableValue(obj)
+        self.actionbox = HBox([
+            self.namingbox,
+            Separator('.'),
+            self.searchbox,
+            Separator('('),
+            self.inputbox,
+            self.inputbutton,
+            Separator(')'),
+            self.gobutton
+        ])
+        self.outputbox = ExplorerOutput(obj)
         self.helpbox = ExplorerHelp(obj)
-        self.bottom = HBox([self.actionbox, self.outputbox, self.helpbox])
+
+        self.bottom = VBox([self.actionbox, self.outputbox, self.helpbox])
+
         self.children = (self.top, self.bottom)
         self.history = []
         self.set_value(obj)
